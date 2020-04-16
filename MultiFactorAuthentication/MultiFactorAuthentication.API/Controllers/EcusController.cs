c4d92b1cbcaa7efdbd9fdf055a2763dbfc5a0ea1
Daakni <daakni@gmail.com>
2020-04-16 16:48:47 +0200

    Added controller GET

---
+++ MultiFactorAuthentication/MultiFactorAuthentication.API/Controllers/EcusController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultiFactorAuthentication.API.Services;
+
+namespace MultiFactorAuthentication.API.Controllers
+{
+    [ApiController]
+    [Route("api/ecus")]
+    public class EcusController : ControllerBase
+    {
+        private readonly IEcuData ecuData;
+
+        public EcusController(IEcuData ecuData)
+        {
+            this.ecuData = ecuData;
+
+        }
+
+        [HttpGet()]
+        public IActionResult GetEcus()
+        {
+            var ecus = ecuData.GetAll();
+            return new JsonResult(ecus);
+        }
+    }
+}
--- MultiFactorAuthentication/MultiFactorAuthentication.API/Services/IEcuData.cs
+++ MultiFactorAuthentication/MultiFactorAuthentication.API/Services/IEcuData.cs
@@ -6,7 +6,7 @@ using MultiFactorAuthentication.API.Models;
 
 namespace MultiFactorAuthentication.API.Services
 {
-    interface IEcuData
+    public interface IEcuData
     {
         Ecu Create(Ecu newEcu);
         IEnumerable<Ecu> GetAll();
