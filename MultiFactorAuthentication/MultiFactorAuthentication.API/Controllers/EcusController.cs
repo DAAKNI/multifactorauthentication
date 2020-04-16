a251578490b17cd1b7c2985bdfea15750534aab1
Daakni <daakni@gmail.com>
2020-04-16 19:48:06 +0200

    Added controller UPDATE, POST, DELETE

--- MultiFactorAuthentication/MultiFactorAuthentication.API/Controllers/EcusController.cs
+++ MultiFactorAuthentication/MultiFactorAuthentication.API/Controllers/EcusController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MultiFactorAuthentication.API.Models;
 using MultiFactorAuthentication.API.Services;
 
 namespace MultiFactorAuthentication.API.Controllers
@@ -19,11 +20,56 @@ namespace MultiFactorAuthentication.API.Controllers
 
         }
 
+        [HttpPost()]
+        public IActionResult CreateEcu(Ecu newEcu)
+        {
+            var ecu = ecuData.Create(newEcu);
+            return Ok(ecu);
+        }
+
         [HttpGet()]
         public IActionResult GetEcus()
         {
             var ecus = ecuData.GetAll();
-            return new JsonResult(ecus);
+            return Ok(ecus);
         }
+
+        [HttpGet("{ecuId}")]
+        public IActionResult GetEcu(int ecuId)
+        {
+            var ecu = ecuData.GetById(ecuId);
+            if (ecu == null)
+            {
+                return NotFound();
+            }
+            return Ok(ecu);
+        }
+
+
+        [HttpPut()]
+        public ActionResult UpdateEcu(Ecu updatedEcu)
+        {
+            var ecu = ecuData.Update(updatedEcu);
+            if (ecu == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ecu);
+        }
+
+        [HttpDelete("{ecuId}")]
+        public ActionResult DeleteEcu(int ecuId)
+        {
+            var ecu = ecuData.Delete(ecuId);
+            if (ecu == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ecu);
+        }
+
+
     }
 }
--- MultiFactorAuthentication/MultiFactorAuthentication.API/Startup.cs
+++ MultiFactorAuthentication/MultiFactorAuthentication.API/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +28,11 @@ namespace MultiFactorAuthentication.API
         {
             services.AddSingleton<IEcuData, InMemoryEcuData>();
             //services.AddScoped<IEcuData, InMemoryEcuData>();
-            services.AddControllers();
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+
+            }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
