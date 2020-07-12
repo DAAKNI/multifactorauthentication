46fc2c3e3d0cf248fe46e75310271df1337cc33d
Daakni <daakni@gmail.com>
2020-07-12 15:43:13 +0200

    Set TwoFactorMethod for Authenticator

--- MultiFactorAuthentication/MultiFactorAuthentication.Web/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ MultiFactorAuthentication/MultiFactorAuthentication.Web/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -99,7 +99,15 @@ namespace MultiFactorAuthentication.Web.Areas.Identity.Pages.Account.Manage
 
             await _userManager.SetTwoFactorEnabledAsync(user, true);
             var userId = await _userManager.GetUserIdAsync(user);
-            _logger.LogInformation("User with ID '{UserId}' has enabled 2FA with an authenticator app.", userId);
+          
+
+            // Update the Two Factor Method of the user
+            user.TwoFactorMethod = TwoFactorType.Authenticator;
+            await _userManager.UpdateAsync(user);
+
+        _logger.LogInformation("User with ID '{UserId}' has enabled 2FA with an authenticator app.", userId);
+
+
 
             StatusMessage = "Your authenticator app has been verified.";
 
